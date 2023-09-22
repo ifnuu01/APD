4679995f295bc8353f4d6a3a50198b1379470cbf
ifnuu01 <110227226+ifnuu01@users.noreply.github.com>
2023-09-22 19:00:00 +0800

    Update postest1.py

--- postest1.py
+++ postest1.py
@@ -1,45 +1,34 @@
-print("Nama : Ifnu Umar")
-print("NIM  : 2309106060")
-print("Program konversi rupiah ke Dollar , Yen ,dan Euro")
+class Customer:
+    def __init__(self, name, age, total_purchase):
+        self.name = name
+        self.age = age
+        self.total_purchase = total_purchase
+        self.discount = 0
 
-def Konversi_dollar(rupiah):
-    usd = 0.000065
-    return rupiah * usd
-def Konversi_yen(rupiah):
-    yen = 0.0096
-    return rupiah * yen 
-def Konversi_euro(rupiah):
-    euro = 0.00006
-    return rupiah * euro
+    def calculate_discount(self):
+        if self.age >= 60 and self.total_purchase >= 120000:
+            self.discount = 0.5
+        elif self.age >= 50 and self.total_purchase >= 100000:
+            self.discount = 0.35
+        elif self.age >= 40 and self.total_purchase >= 70000:
+            self.discount = 0.2
+
+    def display_discount(self):
+        print(f"{self.name} mendapatkan diskon sebesar {self.discount * 100}%")
+        print(f"{self.name} maka harga yang harus di bayar adalah Rp {self.total_purchase -(self.discount * self.total_purchase)}")
 
 
-while True:
-    try:
-        rupiah = float(input("Masukan nominal Rupiah : "))
-        print(f"Nominal yang anda masukan Rp {rupiah}")
-        print()
-        if rupiah > 0 :
-            while True:
-                konver = input("Pilih konversi yang di inginkan untuk Dollar,Yen dan Euro ketik (USD/YEN/EURO) : ").upper()
-                if konver == "USD" or konver =="YEN" or konver =="EURO":
-                    if konver == "USD":
-                        dol = Konversi_dollar(rupiah)
-                        print(f"Nilai hasil konversi dari Rupiah Rp {rupiah} ke Dollar menjadi {dol} US$")
-                    elif konver == "YEN":
-                        jep = Konversi_yen(rupiah)
-                        print(f"Nilai hasil konversi dari Rupiah Rp {rupiah} ke Yen menjadi {jep} ¥")
-                    elif konver == "EURO":
-                        eur = Konversi_euro(rupiah)
-                        print(f"Nilai hasil konversi dari Rupiah Rp {rupiah} ke Euro menjadi {eur} €")
-                    konulang = input("Ingin mengkonversi yang lain ? (y/n) :").lower()
-                    if konulang != "y":
-                        break
-                else:
-                    print("Ketik salah satu huruf yang telah di tunjukan untuk mengkonversi rupiah. Coba lagi !!!")
-            ulangi = input("Apakah Anda ingin mengulang program(y/n)? ").lower()
-            if ulangi != "y":
-                break
-        else:
-            print("Masukan nominal yang benar !!")
-    except ValueError:
-        print("Anda salah memasukan nominal !!")
+num_customers = int(input("Masukkan jumlah pelanggan yang belanja: "))
+customers = []
+
+if num_customers >= 5:
+    for i in range(num_customers):
+        print(f"Data pelanggan ke-{i + 1}:")
+        name = input("Nama pelanggan: ")
+        age = int(input("Umur ibu pelanggan: "))
+        total_purchase = float(input("Total harga belanjaan (Rp): "))
+        customer = Customer(name, age, total_purchase)
+        customer.calculate_discount()
+        customer.display_discount()
+else:
+    print("Maaf, tidak ada diskon karena jumlah pelanggan kurang dari 5")
